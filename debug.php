7bbb970601cd38a786c803f69f29579a36f08f3b
dimzon <dimzon541@gmail.com>
2025-01-01 14:33:31 +0300

    .

--- debug.php
+++ debug.php
@@ -1,4 +1,4 @@
-<?
+<?php
 require_once( __DIR__ . "/make.php");
 makeJson(true);
 
--- make.php
+++ make.php
@@ -14,7 +14,7 @@ function makeJson($debug){
         file_put_contents("tv-config-debug.json", json_encode($template, JSON_PRETTY_PRINT+JSON_UNESCAPED_SLASHES));
     } else {
         $remoteRuleSet = array("type"=>"remote", "tag"=>"tv", "update_interval"=>"2h",
-        "download_detour"=> "proxy", "url"=>"https://raw.githubusercontent.com/dimzon/tvbox-singbox-config/refs/heads/main/tv-ruleset.srs", "format"=>"binary");
+        "download_detour"=> "proxy", "url"=>"https://raw.githubusercontent.com/dimzon/tvbox-singbox-config/main/tv-ruleset.srs", "format"=>"binary");
         $template->route->rule_set[]=$remoteRuleSet;
         file_put_contents("tv-config-release.json", json_encode($template, JSON_PRETTY_PRINT+JSON_UNESCAPED_SLASHES));
     }
